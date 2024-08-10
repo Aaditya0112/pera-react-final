735afd00454e9760cb30648077deaa4723468b0e
Aaditya0112 <rajaaditya0112@gmail.com>
2024-08-10 13:41:15 +0530

    up3

--- src/assets/pages/Committee.jsx
+++ src/assets/pages/Committee.jsx
@@ -11,7 +11,7 @@ function Committee() {
             <h1 className='text-center'>Current Members</h1>
             <div className="member-card w-4/5 flex flex-col lg:w-2/5 items-center justify-center rounded-xl  lg:flex-row  shadow-xl p-2 m-8 ">
             <div className="line flex member-card__image h-56 w-56 lg:mr-6 rounded-full border-8" >
-                <img src={gmv} alt="Prof's Image"/>
+                <img src={gmv} alt="Prof"/>
             </div>
             <div className="member-card__info text-center">
                 <h2>Dr. Guru Raj <i style={{fontSize : '24px', color :'#0a66c2', cursor:'pointer'
@@ -26,7 +26,7 @@ function Committee() {
                 <p className="mailId m-0">
                 <a href="mailto:gururajmv@iitk.ac.in">gururajmv@iitk.ac.in</a>
                 </p>
-                <button className='hoverBlack m-1 p-1 rounded border-2 border-red-400 text-red w-fit ' > <a href="https://home.iitk.ac.in/~gururajmv/" target='_blank' style={{textDecoration:"None", color:"#ff1981"}}>Homepage</a></button>
+                <button className='hoverBlack m-1 p-1 rounded border-2 border-red-400 text-red w-fit ' > <a href="https://home.iitk.ac.in/~gururajmv/" target='_blank' rel="noreferrer" style={{textDecoration:"None", color:"#ff1981"}}>Homepage</a></button>
             </div>
 
         </div>
