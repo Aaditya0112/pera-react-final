8dd35009f67d874afde283333e0353e6c1da00a1
Aaditya0112 <rajaaditya0112@gmail.com>
2024-08-22 15:53:36 +0530

    regUp

--- src/assets/pages/Registration.jsx
+++ src/assets/pages/Registration.jsx
@@ -1,54 +1,70 @@
 import "../stylesheets/registration.css";
 
 const Registration = () => {
-    return(
-        <>
+    return (
+        <div className="containAll flex flex-col">
             <div className="heading">
-        <h1> Registration Fee Details </h1>
-    </div>
-    <div className="ratesTable">
-        <table className="fixed_headers">
-            <thead>
-                <tr>
-                    <th>Category</th>
-                    <th>With Accomodation</th>
-                    <th>Without Accomodation </th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>IEEE PES Student Members</td>
-                    <td>&#8377; 2500</td>
-                    <td>&#8377; 1500</td>
-                </tr>
-                <tr>
-                    <td>Students Non-members</td>
-                    <td>&#8377; 3000</td>
-                    <td>&#8377; 2000</td>
-                </tr>
-                <tr>
-                    <td>Faculty/Industry</td>
-                    <td>&#8377; 10000 <br/><span class = "text-sm font-light">(Only 10 Seats Available)</span></td>
-                    <td>&#8377; 5000</td>
-                </tr>
-                <tr>
-                    <td>Special Category<br/>(Female IEEE PES Student)</td>
-                    <td>&#8377; 1000</td>
-                    <td>&#8377; 500</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+                <h1> Registration Fee Details </h1>
+            </div>
+            <div className="ratesTable">
+                <table className="fixed_headers">
+                    <thead>
+                        <tr>
+                            <th>Category</th>
+                            <th>With Accomodation</th>
+                            <th>Without Accomodation </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>IEEE PES Student Members</td>
+                            <td>&#8377; 2500</td>
+                            <td>&#8377; 1500</td>
+                        </tr>
+                        <tr>
+                            <td>Students Non-members</td>
+                            <td>&#8377; 3000</td>
+                            <td>&#8377; 2000</td>
+                        </tr>
+                        <tr>
+                            <td>Faculty/Industry</td>
+                            <td>&#8377; 10000 <br /><span class="text-sm font-light">(Only 10 Seats Available)</span></td>
+                            <td>&#8377; 5000</td>
+                        </tr>
+                        <tr>
+                            <td>Special Category<br />(Female IEEE PES Student)</td>
+                            <td>&#8377; 1000</td>
+                            <td>&#8377; 500</td>
+                        </tr>
+                    </tbody>
+                </table>
 
-    <div className="form">
-        <a href = "https://forms.gle/Eqoe7Ua85c44UW4e7" target="_blank"> Click Here For Registration</a>
-    </div>
+            </div>
+            <div className="guidelines ">
+                <div >
+                    <ul >
+                        <li>
+                            <span className="text-red-600 text-xl">*</span>Partial travel support is available for the first 50 outstation student registrations.
+                        </li>
+                        <li>
+                        <span className="text-red-600  text-xl">*</span>Limited seats are available in each category
+                        </li>
+                        <li>
+                        <span className="text-red-600  text-xl">*</span>IITK students and faculty are exempt from the registration fee.
+                        </li>
+                    </ul></div>
+            </div>
 
-    {/* <div className="heading">
+
+            <div className="form">
+                <a href="https://forms.gle/Eqoe7Ua85c44UW4e7" target="_blank"> Click Here For Registration</a>
+            </div>
+
+            {/* <div className="heading">
         <h1> Registration Instructions </h1>
     </div> */}
-    <script src="Registration.js"></script>
-        </>
+            <script src="Registration.js"></script>
+        </div>
     )
 }
 
