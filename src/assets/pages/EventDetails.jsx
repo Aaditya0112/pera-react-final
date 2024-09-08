b0181d41913a5138678c7ef2123636d3a98cb7f9
Aaditya0112 <rajaaditya0112@gmail.com>
2024-09-09 00:47:19 +0530

    resEvD

--- src/assets/pages/EventDetails.jsx
+++ src/assets/pages/EventDetails.jsx
@@ -9,19 +9,19 @@ const EventDetails = () => {
                 <h2 className="text-2xl text-white text-center font-bold mb-4 bg-gradient-to-l from-blue-500 to-red-100 p-2 w-full"> Day 1 </h2>
             </div>
             <div className="flex justify-center">
-                <img src= {d1}  className = "h-4/5 w-2/5"alt ='DAY 1'/>
+                <img src= {d1}  className = "h-4/5 w-11/12 md:2/5 lg:w-2/5"alt ='DAY 1'/>
             </div>
             <div className="mb-8 mt-5 flex flex-col items-center ">
                 <h2 className="text-2xl text-white text-center font-bold mb-4 bg-gradient-to-l from-blue-500 to-red-100 p-2 w-full"> Day 2 </h2>
             </div>
             <div className="flex justify-center ">
-                <img src= {d2} className = "h-4/5 w-2/5"alt ='DAY 2'/>
+                <img src= {d2} className = "h-4/5 w-11/12 md:2/5 lg:w-2/5"alt ='DAY 2'/>
             </div>
             <div className="mb-8 mt-5 flex flex-col items-center ">
                 <h2 className="text-2xl text-white text-center font-bold mb-4 bg-gradient-to-l from-blue-500 to-red-100 p-2 w-full"> Day 3 </h2>
             </div>
             <div className="flex justify-center">
-                <img src= {d3} className="h-4/5 w-2/5" alt ='DAY 3'/>
+                <img src= {d3} className="h-4/5 w-11/12 md:2/5 lg:w-2/5" alt ='DAY 3'/>
             </div>
         </div>
     );
